d04c7b3e366e265e86049cd443b9ea30cbe55a98
ryosuke1212 <ryosuke.asa.0909@gmail.com>
2023-10-26 20:36:43 +0900

    Add: コメント機能実装

--- my-first-blog-youtube/microblog/blog/forms.py
+++ my-first-blog-youtube/microblog/blog/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .models import Comment
 
-class Cmmentform(forms.ModelForm):
+class CommentForm(forms.ModelForm):
   class Meta:
     model = Comment
-    model = ""
     fields = ["name", "email", "body"]
